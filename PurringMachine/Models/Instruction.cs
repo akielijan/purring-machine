cf1220776314eddc39a0535d2e3a081216d19d36
Adrian Kielijan <akielijan@interia.pl>
2018-06-01 12:18:57 +0200

    First version of TM

---
+++ PurringMachine/Models/Instruction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PurringMachine.Models
+{
+    public class Instruction
+    {
+        public char symbol;
+        public string state;
+        public char symbolToWrite;
+        public string nextState;
+        public Movement movement;
+
+        public Instruction(char symbol, string state, char nextSymbol, string nextState, Movement movement)
+        {
+            this.symbol = symbol;
+            this.state = state;
+            this.symbolToWrite = nextSymbol;
+            this.nextState = nextState;
+            this.movement = movement;
+        }
+    }
+}
---
+++ PurringMachine/Models/PurringMachine.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PurringMachine.Models
+{
+    public class PurringMachine
+    {
+        private static readonly string NEUTRAL_FINISH_STATE_SYMBOL = "SK";
+        private static readonly string POSITIVE_FINISH_STATE_SYMBOL = "SA";
+        private static readonly string NEGATIVE_FINISH_STATE_SYMBOL = "SN";
+        private static readonly string START_STATE_SYMBOL = "q0";
+        private static readonly char EMPTY_SYMBOL = '#';
+        private static readonly int NO_TAPE = -1;
+
+        private string currentState;
+        private int currentPositionOnTape;
+        private List<char> tape;
+        private List<Instruction> instructions;
+        private bool startFromLeft;
+
+        public PurringMachine()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            currentState = START_STATE_SYMBOL;
+            instructions = new List<Instruction>();
+            tape = new List<char>();
+            currentPositionOnTape = NO_TAPE;
+        }
+
+        public void SetInstructions(List<Instruction> i, bool fromLeft)
+        {
+            instructions = new List<Instruction>(i);
+            startFromLeft = fromLeft;
+        }
+
+        public void ClearInstructions()
+        {
+            instructions.Clear();
+        }
+
+        private int FindFirstNonEmptyElement(bool fromLeft)
+        {
+            if (fromLeft)
+            {
+                for (int i = 0; i < tape.Count(); i++)
+                    if (tape.ElementAt(i) != EMPTY_SYMBOL)
+                        return i;
+            }
+            else
+            {
+                for (int i = tape.Count() - 1; i >= 0; i--)
+                    if (tape.ElementAt(i) != EMPTY_SYMBOL)
+                        return i;
+            }
+
+            return NO_TAPE;
+        }
+
+        public void SetTapeData(List<char> data)
+        {
+            tape = new List<char>(data);
+        }
+
+        public void AddTapeData(char symbol, bool toEnd)
+        {
+            if (toEnd)
+                tape.Add(symbol);
+            else
+                tape.Insert(0, symbol);
+        }
+
+        public void ClearTape()
+        {
+            tape.Clear();
+            currentPositionOnTape = NO_TAPE;
+        }
+
+
+        public void Run()
+        {
+            currentPositionOnTape = FindFirstNonEmptyElement(startFromLeft);
+            if (currentPositionOnTape == NO_TAPE)
+                return;
+
+            while (!IsFinished())
+            {
+                ProcessInstruction();
+            }
+        }
+
+        public void ProcessInstruction()
+        {
+            try
+            {
+                Instruction instruction = instructions.Where(i => i.symbol == tape[currentPositionOnTape] && i.state == currentState).First();
+                tape[currentPositionOnTape] = instruction.symbolToWrite;
+                currentState = instruction.nextState;
+                Move(instruction.movement);
+            }
+            catch (InvalidOperationException) //there is no instruction for current state and symbol
+            {
+                currentState = NEUTRAL_FINISH_STATE_SYMBOL;
+            }
+        }
+
+        public void Move(Movement move)
+        {
+            switch (move)
+            {
+                case Movement.R:
+                    {
+                        currentPositionOnTape++;
+                        if (currentPositionOnTape == tape.Count())
+                            AddTapeData(EMPTY_SYMBOL, true);
+                        break;
+                    }
+                case Movement.L:
+                    {
+                        if (currentPositionOnTape > 0)
+                            currentPositionOnTape--;
+                        else
+                            AddTapeData(EMPTY_SYMBOL, false);
+                        break;
+                    }
+            }
+        }
+
+        public void Reset()
+        {
+            currentState = START_STATE_SYMBOL;
+            currentPositionOnTape = FindFirstNonEmptyElement(startFromLeft);
+        }
+
+        public bool IsFinished()
+        {
+            return IsFinishStatePositive() || IsFinishStateNegative() || IsFinishStateNeutral();
+        }
+
+        public bool IsFinishStatePositive()
+        {
+            return currentState == POSITIVE_FINISH_STATE_SYMBOL;
+        }
+
+        public bool IsFinishStateNegative()
+        {
+            return currentState == NEGATIVE_FINISH_STATE_SYMBOL;
+        }
+
+        public bool IsFinishStateNeutral()
+        {
+            return currentState == NEUTRAL_FINISH_STATE_SYMBOL;
+        }
+
+        public List<char> getTape()
+        {
+            return tape;
+        }
+    }
+}
