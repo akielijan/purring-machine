a422427253a0ab2fac29668310be037466dc174c
Adrian Kielijan <akielijan@interia.pl>
2018-06-02 19:39:43 +0200

    Added basic button bindings

--- PurringMachine/Scripts/machine-script.js
+++ PurringMachine/Scripts/machine-script.js
@@ -3,3 +3,30 @@
         $(this).height($(this).width());
     });
 });
+
+$('#run').on('click', function () {
+    $.get({
+        url: '/Home/RunMachine',
+        success: function (retval) {
+            console.log(retval.Data);
+        }
+    });
+});
+
+$('#reset').on('click', function () {
+    $.get({
+        url: '/Home/ResetMachine',
+        success: function (data, textStatus, xhr) {
+            console.log("success: " + xhr.status);
+        }
+    });
+});
+
+$('#nextStep').on('click', function () {
+    $.get({
+        url: '/Home/NextStep',
+        success: function (data, textStatus, xhr) {
+            console.log("success: " + xhr.status);
+        }
+    });
+});
