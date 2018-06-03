7d2d3dc414efe01bba2b2a810d50ffe554f9fb69
Adrian Kielijan <akielijan@interia.pl>
2018-06-03 19:17:34 +0200

    Added (and tested) different instruction set.

--- PurringMachine/Models/PurringMachine.cs
+++ PurringMachine/Models/PurringMachine.cs
@@ -127,28 +127,28 @@ namespace PurringMachine.Models
             switch (move)
             {
                 case Movement.R:
-                {
-                    CurrentPositionOnTape++;
-                    if (CurrentPositionOnTape == Tape.Count)
                     {
-                        AddTapeData(EMPTY_SYMBOL, true);
-                    }
+                        CurrentPositionOnTape++;
+                        if (CurrentPositionOnTape == Tape.Count)
+                        {
+                            AddTapeData(EMPTY_SYMBOL, true);
+                        }
 
-                    break;
-                }
+                        break;
+                    }
                 case Movement.L:
-                {
-                    if (CurrentPositionOnTape > 0)
                     {
-                        CurrentPositionOnTape--;
-                    }
-                    else
-                    {
-                        AddTapeData(EMPTY_SYMBOL, false);
-                    }
+                        if (CurrentPositionOnTape > 0)
+                        {
+                            CurrentPositionOnTape--;
+                        }
+                        else
+                        {
+                            AddTapeData(EMPTY_SYMBOL, false);
+                        }
 
-                    break;
-                }
+                        break;
+                    }
             }
         }
 
@@ -184,7 +184,7 @@ namespace PurringMachine.Models
             return Tape;
         }
 
-        private static List<Instruction> GetDefaultInstructionSet()
+        private static List<Instruction> GetIncrementBinaryValueInstructionSet()
         {
             return new List<Instruction>
             {
@@ -198,11 +198,69 @@ namespace PurringMachine.Models
             };
         }
 
+        private static List<Instruction> GetDoubleCharactersInTapeInstructionSet()
+        {
+            return new List<Instruction>
+            {
+                new Instruction('#',"q0",'#',"q0",Movement.R),
+                new Instruction('a',"q0",'#',"q1",Movement.R),
+                new Instruction('b',"q0",'#',"q7",Movement.R),
+
+                new Instruction('#',"q1",'#',"q2",Movement.R),
+                new Instruction('a',"q1",'a',"q1",Movement.R),
+                new Instruction('b',"q1",'b',"q1",Movement.R),
+
+                new Instruction('#',"q2",'a',"q3",Movement.R),
+                new Instruction('a',"q2",'a',"q2",Movement.R),
+                new Instruction('b',"q2",'b',"q2",Movement.R),
+
+                new Instruction('#',"q3",'a',"q4",Movement.L),
+                new Instruction('a',"q3",'a',"SK",Movement.R),
+                new Instruction('b',"q3",'b',"SK",Movement.R),
+
+                new Instruction('#',"q4",'#',"q5",Movement.L),
+                new Instruction('a',"q4",'a',"q4",Movement.L),
+                new Instruction('b',"q4",'b',"q4",Movement.L),
+
+                new Instruction('#',"q5",'#',"SK",Movement.R),
+                new Instruction('a',"q5",'a',"q6",Movement.L),
+                new Instruction('b',"q5",'b',"q6",Movement.L),
+
+                new Instruction('#',"q6",'#',"q0",Movement.R),
+                new Instruction('a',"q6",'a',"q6",Movement.L),
+                new Instruction('b',"q6",'b',"q6",Movement.L),
+
+                new Instruction('#',"q7",'#',"q8",Movement.R),
+                new Instruction('a',"q7",'a',"q7",Movement.R),
+                new Instruction('b',"q7",'b',"q7",Movement.R),
+
+                new Instruction('#',"q8",'b',"q9",Movement.R),
+                new Instruction('a',"q8",'a',"q8",Movement.R),
+                new Instruction('b',"q8",'b',"q8",Movement.R),
+
+                new Instruction('#',"q9",'b',"q10",Movement.L),
+                new Instruction('a',"q9",'a',"SK",Movement.R),
+                new Instruction('b',"q9",'b',"SK",Movement.R),
+
+                new Instruction('#',"q10",'#',"q11",Movement.L),
+                new Instruction('a',"q10",'a',"q10",Movement.L),
+                new Instruction('b',"q10",'b',"q10",Movement.L),
+
+                new Instruction('#',"q11",'#',"SK",Movement.R),
+                new Instruction('a',"q11",'a',"q12",Movement.L),
+                new Instruction('b',"q11",'b',"q12",Movement.L),
+
+                new Instruction('#',"q12",'#',"q0",Movement.R),
+                new Instruction('a',"q12",'a',"q12",Movement.L),
+                new Instruction('b',"q12",'b',"q12",Movement.L)
+            };
+        }
+
         public static PurringMachine GetDefaultMachine()
         {
             PurringMachine m = new PurringMachine();
             const bool fromLeft = false;
-            m.SetInstructions(GetDefaultInstructionSet(), fromLeft);
+            m.SetInstructions(GetIncrementBinaryValueInstructionSet(), fromLeft);
             m.SetTapeData("1111");
             m.Reset();
             return m;
